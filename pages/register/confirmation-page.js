9fa4560d264a28721c4bd338d8284c1f93d3aba6
Pschaefer18 <paul.r.schaefer11@gmail.com>
2024-02-22 11:55:51 -0500

    added routing and cleaned up form

---
+++ pages/register/confirmation-page.js
@@ -0,0 +1,18 @@
+import styles from './confirmation-page.css'
+import { useRouter } from 'next/router'
+
+export default function confirmationPage() {
+    const router = useRouter()
+    const amount = router.query.amount
+    return (
+        <div class='body'>
+            <h2>Your form has been submitted</h2>
+            <h3>Thank you for signing up!</h3>
+            <p>Please write a check of ${amount} to Paul Schaefer by May 1st</p>
+            <p>Mail to the following address or hand deliver to me</p>
+            <p>4161 Sunset Ct, Ann Arbor, MI 48103</p>
+            <h3>If you decide to cancel your membership</h3>
+            <p>please email hometownharvestllc@gmail.com</p>
+        </div>
+    )
+}
