3f1e3386afe548f9ecb505b60177223980013707
Pschaefer18 <paul.r.schaefer11@gmail.com>
2024-05-01 22:36:55 -0400

    fixed document error by importing bootstrap js differently

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ import {db} from "./firebase"
 import {collection, addDoc} from 'firebase/firestore';
 import { useState, useRef, useEffect } from "react";
 import Link from "next/link";
-import bootstrap from 'bootstrap';
 import Carousel from "../components/Carousel";
 
 export default function Home() {
@@ -21,15 +20,10 @@ export default function Home() {
     { src: '/IMG_1414.JPEG', alt: 'Image 1', title: 'Slide 1', description: 'This is the first slide description' }
     // ... other image objects
   ];
-
   return (
     <>
     <Head>
       <title>Homepage</title>
-      <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    />
     <link
       rel="stylesheet"
       href="/page.module.css"
@@ -41,6 +35,7 @@ export default function Home() {
           <h2>Website Coming Soon</h2>
           <h4>Register today using the button below</h4>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <div class="carousel-container">
           <Carousel images={images} />
         </div>
