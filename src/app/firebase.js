fc4a6c1dd8a49a0ae211bfea3bb330d0241b19a4
Paul Schaefer <paul.r.schaefer11@gmail.com>
2025-02-16 22:20:56 -0500

    correction

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,6 +1,5 @@
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
-const app = initializeApp(firebaseConfig);
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -11,4 +10,5 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
