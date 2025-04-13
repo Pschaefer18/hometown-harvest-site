7711a479d48fa3b1340799d10955d82b60fc521c
Paul Schaefer <paul.r.schaefer11@gmail.com>
2025-04-13 09:36:04 -0400

    improved presentation when link shared

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,21 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Hometown Harvest CSA",
-  description: "24 week CSA program in Dexter, MI",
+  title: "Hometown Harvest",
+  description: "CSA Market Garden in Dexter, MI",
+  openGraph: {
+    title: "Hometown Harvest",
+    description: "CSA Market Garden in Dexter, MI",
+    url: "https://www.hometownharvestllc.com/", // Your deployed URL
+    images: [
+      {
+        url: "/Users/paulschaefer/Documents/hometown-harvest-site/public/Hometown Harvest Logo.png", // Absolute URL to image
+        width: 1200,
+        height: 630,
+        alt: "My App Preview",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
